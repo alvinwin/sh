23c92d6b55aaaa9679c58c58d7a55d7f6b057b23
alvinwin <106125101+alvinwin@users.noreply.github.com>
2023-07-04 18:45:47 -0700

    Update protonvpn-reswitch.sh

--- protonvpn-reswitch.sh
+++ protonvpn-reswitch.sh
@@ -1,5 +1,15 @@
+# disconnect
 protonvpn-cli d
+
+# killswitch off
 protonvpn-cli ks --off
+
+# connect to the fastest available vpn server
 protonvpn-cli c -f
+
+# killswitch on
 protonvpn-cli ks --on
+
+# reconnects to the selected server
+# killswitch must be enabled when a connection is established to be active
 protonvpn-cli r
