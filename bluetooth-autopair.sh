19b95f1592de5c0340495e7ed2f5d73e38186334
alvinwin <106125101+alvinwin@users.noreply.github.com>
2023-07-05 13:15:38 -0700

    Update bluetooth-autopair.sh
    added pair statements

--- bluetooth-autopair.sh
+++ bluetooth-autopair.sh
@@ -5,6 +5,10 @@ bluetoothctl remove AA:BB:CC:DD:EE:FF
 # scan for available bluetooth devices 
 bluetoothctl scan on
 
+# pair with devices
+bluetoothctl pair 00:11:22:33:44:55
+bluetoothctl pair AA:BB:CC:DD:EE:FF
+
 # connect to devices
 blueconnectctl connect 00:11:22:33:44:55
 bluetoothctl connect AA:BB:CC:DD:EE:FF
