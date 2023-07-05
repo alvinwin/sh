b432640398f5c0c7ea91d20a0e99c01d9fc65b6a
alvinwin <106125101+alvinwin@users.noreply.github.com>
2023-07-04 18:49:38 -0700

    Update bluetooth-autopair.sh

--- bluetooth-autopair.sh
+++ bluetooth-autopair.sh
@@ -1,4 +1,11 @@
-scan on
-connect 40:58:99:2E:24:31
-connect A4:AE:12:AB:AC:D4
-scan off
+# scan for available bluetooth devices 
+bluetoothctl scan on
+
+# connect to headphones
+blueconnectctl connect 40:58:99:2E:24:31
+
+# connect to controller
+bluetoothctl connect A4:AE:12:AB:AC:D4
+
+# disables bluetooth device scanning
+bluetoothctl scan off
